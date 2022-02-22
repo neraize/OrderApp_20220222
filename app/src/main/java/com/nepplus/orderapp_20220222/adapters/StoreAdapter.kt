3adf0bb9ae7bb7ecb1b0fc81fe6d846adc032476
seojungmin <neraize@gmail.com>
2022-02-22 11:01:49 +0900

    getView함수 기본구조

--- app/src/main/java/com/nepplus/orderapp_20220222/adapters/StoreAdapter.kt
+++ app/src/main/java/com/nepplus/orderapp_20220222/adapters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.nepplus.orderapp_20220222.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.orderapp_20220222.R
 import com.nepplus.orderapp_20220222.datas.StoreData
 
 class StoreAdapter(
@@ -10,5 +14,16 @@ class StoreAdapter(
     val mList:ArrayList<StoreData>
 ):ArrayAdapter<StoreData>(mContext, resId, mList) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+        var tempRow = convertView
+        if(tempRow==null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.fragment_pizza_store_list, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
 }
