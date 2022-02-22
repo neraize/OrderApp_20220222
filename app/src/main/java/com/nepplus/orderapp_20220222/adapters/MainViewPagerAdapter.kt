b0222468ea67b028dcf2604531878c32d46b1800
seojungmin <neraize@gmail.com>
2022-02-22 09:55:20 +0900

    1. 액티비티에 ViewPager 태그를 배치 2. 뷰페이저 어댑터 클래스 제작중 - 상속 부분 해결

---
+++ app/src/main/java/com/nepplus/orderapp_20220222/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,8 @@
+package com.nepplus.orderapp_20220222.adapters
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm:FragmentManager):FragmentPagerAdapter(fm) {
+
+}
