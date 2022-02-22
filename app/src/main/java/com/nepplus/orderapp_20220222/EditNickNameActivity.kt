23aadac2eb6408d62cb546f253b9b77cf0776b55
seojungmin <neraize@gmail.com>
2022-02-22 15:45:39 +0900

    닉네임 변경 화면 추가만 함

---
+++ app/src/main/java/com/nepplus/orderapp_20220222/EditNickNameActivity.kt
@@ -0,0 +1,22 @@
+package com.nepplus.orderapp_20220222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNickNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nick_name)
+
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+
+    }
+}
